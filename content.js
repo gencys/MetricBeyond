0aabc5a51d63a01c6352c88bf26fc09972d40a79
gencys <43763900+gencys@users.noreply.github.com>
2023-02-12 22:48:44 +0000

    v1.5

--- content.js
+++ content.js
@@ -23,7 +23,7 @@ function changeToMeters_One(element) {
 }
 
 function changeToMeters() {
-    var distancesNumber, distancesUnit, numberOfDistances, newDistance;
+    var distancesNumber, distancesUnit, numberOfDistances;
     
     if (document.querySelector(".ddbc-distance-number")) {
         distancesNumber = document.querySelectorAll(".ddbc-distance-number__number");
@@ -79,7 +79,7 @@ function changeItemRange() {
 }
 
 function changeToKilograms_One(elementNumber, elementUnit) {
-    elementNumber.textContent = (parseFloat(elementNumber.textContent) *1000)/ 2000;
+    elementNumber.textContent = (parseFloat(elementNumber.textContent) * 1000 ) / 2000;
     elementUnit.textContent = "kg.";
 }
 
@@ -97,16 +97,16 @@ function changeToKilograms() {
     
     if (document.querySelector(".ct-equipment__container-weight-capacity")) {
         weightContainerCapacity = document.querySelectorAll(".ct-equipment__container-weight-capacity");
-        var regexWeigthContainerCapacity = /\((\d+)\/(\d+) lb\)/g;
+        var regexWeightContainerCapacity = /\((\d+)\/(\d+) lb\)/g;
         var text, matches, firstNumber, secondNumber;
         weightContainerCapacity.forEach((element) => {
             text = element.textContent;
-            matches = [...text.matchAll(regexWeigthContainerCapacity)];
+            matches = [...text.matchAll(regexWeightContainerCapacity)];
             if (matches.length != 0) {
                 matches.forEach((match) => {
                     firstNumber = (parseFloat(match[1]) *1000)/ 2000;
                     secondNumber = (parseFloat(match[2]) *1000)/ 2000;
-                    text = text.replace(regexWeigthContainerCapacity, "(" + firstNumber  + "/" + secondNumber + " kg)");
+                    text = text.replace(regexWeightContainerCapacity, "(" + firstNumber  + "/" + secondNumber + " kg)");
                 });
                 element.textContent = text;
             }
@@ -115,30 +115,14 @@ function changeToKilograms() {
     }
 }
 
-function changeInText() {
-    var divToSearch = [
-        ".ct-senses__summary",
-        ".jsx-parser",
-        ".ddbc-html-content",
-        ".ct-sidebar__pane",
-        '.ct-sidebar__pane .ddbc-creature-block span[class$="-data"]',
-        ".ddbc-item-name",
-        ".ct-preferences-pane__field-description"
-    ];
+function changeInText(divList) {
     var currDiv;
-    divToSearch.forEach( (divClass) => {
-        currDiv = document.querySelectorAll(divClass + " p");
-        if (divClass == divToSearch[0] || divClass == divToSearch[4] || divClass == divToSearch[5] || divClass == divToSearch[6]) {
-            currDiv = document.querySelectorAll(divClass);
-        }
+    divList.forEach( (divSelector) => {
+        currDiv = document.querySelectorAll(divSelector);
         if ( currDiv ) {
             currDiv.forEach( (element) => {
                 if (!element.classList.contains("checked")) {
-                    changeZoneToMetersInText(element);
-                    changeRangeToMetersInText(element);
-                    changeCompositeToMetersInText(element);
-                    changeWeightToKilogramsInText(element);
-                    changeRangeToCentimetersInText(element);
+                    parseAndChangeInText(element);
                     element.classList.add("checked");
                 }
             });
@@ -146,64 +130,97 @@ function changeInText() {
     });
 }
 
-function changeZoneToMetersInText(element) {
-    var regexZone = /(\d+) by (\d+) ft./g;
+function parseAndChangeInText(element) {
+    var regexDict = [
+        [
+            parseAndChangeZone,
+            [
+                [/(\d+) by (\d+) ft./g, /(\d+) by (\d+) ft./, [" by ", " m."]]
+            ],
+            [30, 100]
+        ],
+        [
+            parseAndChangeZone,
+            [
+                [/(\d+) by (\d+) miles/g, /(\d+) by (\d+) miles/, [" by ", " kilometers"]],
+                [/(\d+) to (\d+) miles/g, /(\d+) to (\d+) miles/, [" to ", " kilometers"]]
+            ],
+            [150, 100]
+        ],
+        [
+            parseAndChangeUnit,
+            [
+                [/\d+ ft\.?/g, /\d+ ft\.?/, " m."],
+                [/\d+ feet/g, /\d+ feet/, " meters"],
+                [/\d+ foot/g, /\d+ foot/, " meters"],
+                [/\d+-\s*foot/g, /\d+-\s*foot/, "-meters"]
+            ],
+            [30, 100]
+        ],
+        [
+            parseAndChangeUnit,
+            [
+                [/\d+ miles/g, /\d+ miles/, " kilometers"],
+                [/\d+ mile/g, /\d+ mile/, " kilometers"],
+                [/\d+-\s*mile/g, /\d+-\s*mile/, "-kilometers"],
+                [/\d+ or more miles/g, /\d+ or more miles/, " or more kilometers"]
+            ],
+            [150, 100]
+        ],
+        [
+            parseAndChangeUnit,
+            [
+                [/\d+ in\.?/g, /\d+ in\.?/, " cm."],
+                [/\d+ inches/g, /\d+ inches/, " centimeters"],
+                [/\d+ inch/g, /\d+ inch/, " centimeters"],
+                [/\d+-\s*inch/g, /\d+-\s*inch/, "-centimeters"]
+            ],
+            [250, 100]
+        ],
+        [
+            parseAndChangeUnit,
+            [
+                [/\d+ cubic foot/g, /\d+ cubic foot/, " cubic meters"],
+                [/\d+ cubic feet/g, /\d+ cubic feet/, " cubic meters"]
+            ],
+            [30, 1000]
+        ],
+        [
+            parseAndChangeUnit,
+            [
+                [/\d+ lb\.?/g, /\d+ lb\.?/, " kg."],
+                [/\d+ pounds/g, /\d+ pounds/, " kilograms"],
+                [/\d+ pound/g, /\d+ pound/, " kilograms"],
+                [/\d+-\s*pounds/g, /\d+-\s*pounds/, "-kilograms"]
+            ],
+            [1000, 2000]
+        ]
+    ];
+    regexDict.forEach( (parsePackage) => {
+        parsePackage[0](element, parsePackage[1], parsePackage[2]);
+    });
+}
+
+function parseAndChangeZone(element, regex, rate) {
     var text = element.textContent;
-    var matchesZone = [...text.matchAll(regexZone)];
-    if (matchesZone != null && matchesZone.length != 0) {
-        var firstNumber, secondNumber;
-        matchesZone.forEach( (match) => {
-            firstNumber = (parseFloat(match[1]) * 30)/100;
-            secondNumber = (parseFloat(match[2]) * 30)/100;
-            text = text.replace(regexZone, firstNumber + " by " + secondNumber + " m.");
-        });
-        element.textContent = text;
-    }
+    var matches, firstNumber, secondNumber;
+    regex.forEach( (regexElement) => {
+        matches = [...text.matchAll(regexElement[0])];
+        if (matches.length != 0) {
+            matches.forEach( (match) => {
+                firstNumber = (parseFloat(match[1]) * rate[0])/rate[1];
+                secondNumber = (parseFloat(match[2]) * rate[0])/rate[1];
+                text = text.replace(regexElement[1], firstNumber + regexElement[2][0] + secondNumber + regexElement[2][1]);
+            });
+            element.textContent = text;
+        }
+    });
 }
 
-function changeRangeToMetersInText(element) {
-    var regexRange = [
-        [/\d+ ft./g, /\d+ ft./, " m."],
-        [/\d+ feet/g, /\d+ feet/, " meters"],
-        [/\d+ foot/g, /\d+ foot/, " meters"],
-        [/\d+-foot/g, /\d+-foot/, "-meters"]
-    ];
-    parseAndChangeText(element, regexRange, [30,100]);
-}
-
-function changeRangeToCentimetersInText(element) {
-    var regexRangeCM = [
-        [/\d+ in\./g, /\d+ in\./, " cm."],
-        [/\d+ inches/g, /\d+ inches/, " centimeters"],
-        [/\d+ inch/g, /\d+ inch/, " centimeters"],
-        [/\d+-inch/g, /\d+-inch/, "-centimeters"]
-    ];
-    parseAndChangeText(element, regexRangeCM, [250,100]);
-}
-
-function changeCompositeToMetersInText(element) {
-    var regexComposite = [
-        [/\d+ cubic foot/g, /\d+ cubic foot/, " cubic meters"],
-        [/\d+ cubic feet/g, /\d+ cubic feet/, " cubic meters"]
-    ];
-    parseAndChangeText(element, regexComposite, [30,1000]);
-}
-
-function changeWeightToKilogramsInText(element) {
-    var regexWeight = [
-        [/\d+ lb\./g, /\d+ lb\./, " kg."],
-        [/\d+ pounds/g, /\d+ pounds/, " kilograms"],
-        [/\d+ pound/g, /\d+ pound/, " kilograms"],
-        [/\d+-pounds/g, /\d+-pounds/, "-kilograms"]
-    ];
-    parseAndChangeText(element, regexWeight, [1000,2000]);
-    
-}
-
-function parseAndChangeText(element, regex, rate) {
+function parseAndChangeUnit(element, regex, rate) {
     var text = element.textContent;
     var matches;
-    regex.forEach( (regexElement, index) => {
+    regex.forEach( (regexElement) => {
         matches = [...text.matchAll(regexElement[0])];
         if (matches.length != 0) {
             matches.forEach( (match) => {
@@ -215,32 +232,37 @@ function parseAndChangeText(element, regex, rate) {
 }
 
 function changeInMonsterPage(){
-    var divToSearch = [".mon-stat-block", ".mon-details__description-block", '.mon-stat-block span[class$="-data"]'];
-    var currDiv;
-    divToSearch.forEach( (divClass) => {
-        currDiv = document.querySelectorAll(divClass + " p");
-        if (divClass == divToSearch[2]) {
-            currDiv = document.querySelectorAll(divClass);
-        }
-        if ( currDiv ) {
-            currDiv.forEach( (element) => {
-                if (!element.classList.contains("checked")) {
-                    changeZoneToMetersInText(element);
-                    changeRangeToMetersInText(element);
-                    changeCompositeToMetersInText(element);
-                    changeWeightToKilogramsInText(element);
-                    changeRangeToCentimetersInText(element);
-                    element.classList.add("checked");
-                }
-            });
-        }
-    });
+    const monsterPageDivs = [
+        ".mon-stat-block p",
+        ".mon-details__description-block p",
+        '.mon-stat-block span[class$="-data"]'
+    ];
+    changeInText(monsterPageDivs);
+}
+
+function changeInOtherPage(){
+    const otherPageDivs = [
+        ".ddb-statblock-item-value",
+        ".more-info-content p",
+        ".more-info-content li"
+    ];
+    changeInText(otherPageDivs);
 }
 
 function changeInCharacterPage() {
-        changeToMeters();
-        changeInText();
-        changeToKilograms();
+    const characterPageDivs = [
+        ".ct-senses__summary",
+        ".jsx-parser p",
+        ".ddbc-html-content p",
+        ".ddbc-html-content li",
+        ".ct-sidebar__pane p",
+        '.ct-sidebar__pane .ddbc-creature-block span[class$="-data"]',
+        ".ddbc-item-name",
+        ".ct-preferences-pane__field-description"
+    ];
+    changeToMeters();
+    changeToKilograms();
+    changeInText(characterPageDivs);
 }
 
 function observeWhenReady() {
@@ -250,13 +272,29 @@ function observeWhenReady() {
         return;
     }
     var url = window.location.href;
-    if (url.match(/characters/g)) {
+    if (url.match(/characters\//g)) {
         const observer = new MutationObserver( changeInCharacterPage );
         observer.observe(observedNode,{childList: true, subtree: true});
-    } else {
+    } else if (url.match(/monsters\//g)) {
         changeInMonsterPage();
+    } else {
+        changeInOtherPage();
     }
     
 }
 
-observeWhenReady();
+function loadPreferencesAndRun(result) {
+    if (result.state == "disabled"){
+        console.log("Metric Beyond is disabled");
+    }
+    else {
+        observeWhenReady();
+    }
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+const getting = browser.storage.sync.get("state");
+getting.then(loadPreferencesAndRun, onError);
---
+++ options/options.js
@@ -0,0 +1,42 @@
+function saveOptions(e) {
+	let stateElem = document.querySelector("#state");
+    let instructions = document.querySelector("#refresh");
+
+    e.preventDefault();
+    if (stateElem.value == "Click to disable") {
+      state = "disabled";
+	  stateElem.value = "Click to enable";
+    }
+    else {
+      state = "enabled";
+	  stateElem.value = "Click to disable";
+    }
+    if ( instructions.style.display == "none") {
+      instructions.style.display = "block";
+    }
+    else {
+      instructions.style.display = "none";
+    }
+    browser.storage.sync.set({
+      state: state
+    });
+}
+  
+function restoreOptions() {
+    function setCurrentChoice(result) {
+    	if (result.state == "disabled") {
+        	document.querySelector("#state").value = "Click to enable";
+    	}
+    }
+  
+    function onError(error) {
+    	console.log(`Error: ${error}`);
+    }
+  
+    let getting = browser.storage.sync.get("state");
+    getting.then(setCurrentChoice, onError);
+}
+  
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("#state").addEventListener("click", saveOptions);
+  
